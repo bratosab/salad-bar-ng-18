5f672c683e2800c0e004b0692ee61195088f626e
Abdallah <bratosab@gmail.com>
2024-10-15 21:13:42 +0200

    feat(ngrx)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from './store/app.reducer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  // private store = inject(Store);
   title = 'salad-bar-ng-18';
+
+  name$!: Observable<string>;
+  orderId$!: Observable<number>;
+
+  constructor(private store: Store<{ app: AppState }>) {
+    this.name$ = this.store.select((state) => state.app.name);
+    this.orderId$ = this.store.select((state) => state.app.orderId);
+  }
 }
