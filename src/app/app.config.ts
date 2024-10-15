c6a6756ea5b908756d043237faae585008de10be
Abdallah <bratosab@gmail.com>
2024-10-15 21:21:44 +0200

    feat(ngrx): installed store with router & devtools plugin

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideStore([], withNgxsReduxDevtoolsPlugin(), withNgxsRouterPlugin()),
   ],
 };
